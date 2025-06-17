252afbbe2640cf7d1bd54ee416d3500e7cca50fc
acidMyke <owner@acidmyke.link>
2025-06-17 22:40:37 +0800

    feat: trpc context with cloudflare parameters

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,15 @@
 // From here call trpc when request hit /trpc
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { appRouter } from './router';
+import { createContextFactory } from './trpc';
 
 export default {
-  fetch: req => fetchRequestHandler({ endpoint: '/trpc', req, router: appRouter }),
+  fetch(req, env, ctx) {
+    return fetchRequestHandler({
+      endpoint: '/trpc',
+      req,
+      router: appRouter,
+      createContext: createContextFactory(env, ctx),
+    });
+  },
 } satisfies ExportedHandler<Env>;
--- server/trpc.ts
+++ server/trpc.ts
@@ -1,6 +1,19 @@
 import { initTRPC } from '@trpc/server';
+import type { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch';
 
-const t = initTRPC.create();
+export function createContextFactory(env: Env, ctx: ExecutionContext) {
+  return function ({ req }: FetchCreateContextFnOptions) {
+    return {
+      req,
+      env, // Cloudflare workers enviroment
+      wctx: ctx, // Cloudflare workers context
+    };
+  };
+}
+
+export type Context = Awaited<ReturnType<ReturnType<typeof createContextFactory>>>;
+
+const t = initTRPC.context<Context>().create();
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
