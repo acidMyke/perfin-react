37a598bd18f039c4ba4a9c95e9fa9b3bf623402a
acidMyke <13977709+acidMyke@users.noreply.github.com>
2025-07-19 06:38:43 +0000

    poc: testing api and page

--- client/App.tsx
+++ client/App.tsx
@@ -1,9 +1,11 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './trpc';
-export function App() {
+import TestPage from './pages/TestPage';
+
+export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <></>
+      <TestPage></TestPage>
     </QueryClientProvider>
   );
 }
