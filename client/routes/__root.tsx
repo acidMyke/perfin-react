24986ca9cba0c6a500f50dad801bd86983b37d7f
acidMyke <owner@acidmyke.link>
2025-07-19 21:46:31 +0800

    feat: navigation bar

--- client/routes/__root.tsx
+++ client/routes/__root.tsx
@@ -1,10 +1,49 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { createRootRoute, Link, linkOptions, Outlet } from '@tanstack/react-router';
 
-export const Route = createRootRoute({
-  component: () => (
+const options = linkOptions([
+  {
+    to: '/',
+    label: 'Home',
+  },
+  {
+    to: '/',
+    label: 'Test',
+  },
+]);
+
+function Navbar() {
+  return (
+    <div className='navbar bg-base-100 shadow-sm'>
+      <div className='navbar-start'>
+        <a className='btn btn-ghost text-xl'>Perfin</a>
+      </div>
+      <div className='navbar-center hidden lg:flex'>
+        <ul className='menu menu-horizontal px-1'>
+          {options.map((options, index) => {
+            return (
+              <li key={index}>
+                <Link className='btn btn-ghost drawer-button' activeProps={{ className: 'btn-active' }} {...options}>
+                  {options.label}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className='navbar-end'></div>
+    </div>
+  );
+}
+
+function Root() {
+  return (
     <>
-      <hr />
+      <Navbar />
       <Outlet />
     </>
-  ),
+  );
+}
+
+export const Route = createRootRoute({
+  component: Root,
 });
