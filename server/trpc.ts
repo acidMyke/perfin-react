9e7812d3b0f0eee7215845fc3f765a35082b589e
acidMyke <owner@acidmyke.link>
2025-07-20 17:23:34 +0800

    feat: protected procedure

--- server/trpc.ts
+++ server/trpc.ts
@@ -1,4 +1,4 @@
-import { initTRPC } from '@trpc/server';
+import { initTRPC, TRPCError } from '@trpc/server';
 import type { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch';
 import { drizzle } from 'drizzle-orm/d1';
 import * as schema from '../db/schema';
@@ -37,4 +37,55 @@ export const publicProcedure = t.procedure.use(async opts => {
   } else {
     return opts.next();
   }
+});
+
+export const protectedProcedure = publicProcedure.use(async opts => {
+  const { req, env, db } = opts.ctx;
+  let token: undefined | string;
+  let failureReason = '';
+  const cookieHeader = req.headers.get('Cookie');
+  if (cookieHeader) {
+    const cookies = cookieHeader.split(';').map(cookie => cookie.trim());
+
+    if (cookies.length === 0) {
+      failureReason = 'Empty cookie header';
+    }
+
+    for (const cookie of cookies) {
+      const [name, ...rest] = cookie.split('=');
+      if (name === env.TOKEN_COOKIE_NAME) {
+        token = decodeURIComponent(rest.join('='));
+      }
+    }
+  } else {
+    failureReason = 'Missing cookie header';
+  }
+
+  if (token) {
+    const session = await db.query.sessionsTable.findFirst({
+      where: (session, { eq, gt, and }) => and(eq(session.token, token), gt(session.expiresAt, new Date())),
+      columns: { createdAt: true, expiresAt: true, lastUsedAt: true },
+      with: {
+        user: { columns: { id: true, name: true } },
+      },
+    });
+
+    if (session) {
+      opts.next({
+        ctx: {
+          user: session.user,
+          session,
+        },
+      });
+    } else {
+      failureReason = 'Unable to find token';
+    }
+  } else {
+    failureReason = 'Missing token';
+  }
+
+  if (import.meta.env.DEV) {
+    throw new TRPCError({ code: 'UNAUTHORIZED', message: failureReason });
+  }
+  throw new TRPCError({ code: 'UNAUTHORIZED' });
 });
