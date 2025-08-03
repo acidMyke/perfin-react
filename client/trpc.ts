c4fb25cf5adafda09ed127a9cc77fffed50bcf11
acidMyke <owner@acidmyke.link>
2025-08-03 21:11:36 +0800

    feat: automatic invalidation when server respond unauthorized

--- client/trpc.ts
+++ client/trpc.ts
@@ -1,11 +1,44 @@
 import { QueryClient } from '@tanstack/react-query';
-import { createTRPCClient, httpBatchLink } from '@trpc/client';
+import { createTRPCClient, httpBatchLink, type TRPCLink } from '@trpc/client';
 import { createTRPCOptionsProxy } from '@trpc/tanstack-react-query';
 import type { AppRouter } from '../server/router';
+import { observable } from '@trpc/server/observable';
 
 export const queryClient = new QueryClient();
+
+const errorHandlingLink: TRPCLink<AppRouter> = () => {
+  // here we just got initialized in the app - this happens once per app
+  // useful for storing cache for instance
+  return ({ next, op }) => {
+    // this is when passing the result to the next link
+    // each link needs to return an observable which propagates results
+    return observable(observer => {
+      const unsubscribe = next(op).subscribe({
+        error(err) {
+          observer.error(err);
+          if (err.data) {
+            const { code } = err.data;
+            if (code === 'UNAUTHORIZED') {
+              queryClient.invalidateQueries({
+                queryKey: trpc.whoami.queryKey(),
+              });
+            }
+          }
+        },
+        next(value) {
+          observer.next(value);
+        },
+        complete() {
+          observer.complete();
+        },
+      });
+      return unsubscribe;
+    });
+  };
+};
+
 const trpcClient = createTRPCClient<AppRouter>({
-  links: [httpBatchLink({ url: '/trpc' })],
+  links: [errorHandlingLink, httpBatchLink({ url: '/trpc' })],
 });
 
 export const trpc = createTRPCOptionsProxy<AppRouter>({
@@ -16,6 +49,6 @@ export const trpc = createTRPCOptionsProxy<AppRouter>({
 // Exposing trpc client to be used in console
 if (import.meta.env.DEV) {
   if (window) {
-Object.assign(window, { trpc, trpcClient });
+    Object.assign(window, { trpc, trpcClient });
   }
 }
