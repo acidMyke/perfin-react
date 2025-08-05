0ee5c2f1a71ac447239d2539eae929872a258d85
acidMyke <13977709+acidMyke@users.noreply.github.com>
2025-08-05 13:24:58 +0000

    feat: sign form field and basic validators

--- client/routes/signin.tsx
+++ client/routes/signin.tsx
@@ -1,3 +1,4 @@
+import { useForm } from '@tanstack/react-form';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/signin')({
@@ -5,17 +6,69 @@ export const Route = createFileRoute('/signin')({
 });
 
 function RouteComponent() {
+  const { Field: SignInFormField } = useForm({
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+    onSubmit: ({ value }) => {
+      console.log('Form submitted', value);
+    },
+  });
+
   return (
     <div className='mx-auto max-w-md'>
       <h1 className='mt-20 text-center text-3xl font-black'>Perfin Sign In</h1>
-      <label className='floating-label mt-12'>
-        <input type='text' placeholder='Username' className='input input-primary input-xl w-full' />
-        <span>Username</span>
-      </label>
-      <label className='floating-label mt-12'>
-        <input type='password' placeholder='Password' className='input input-primary input-xl w-full' />
-        <span>Password</span>
-      </label>
+      <SignInFormField
+        name='username'
+        validators={{ onChange: ({ value }) => (value.length <= 0 ? 'Cannot be empty' : undefined) }}
+      >
+        {field => (
+          <>
+            <label htmlFor={field.name} className='floating-label mt-12'>
+              <input
+                type='text'
+                id={field.name}
+                name={field.name}
+                placeholder='Username'
+                className='input input-primary input-xl w-full'
+                value={field.state.value}
+                onChange={e => field.handleChange(e.target.value)}
+              />
+              <span>Username</span>
+            </label>
+            <p role='alert' className='text-error h-[1em]'>
+              {field.state.meta.errors.join(', ')}
+            </p>
+          </>
+        )}
+      </SignInFormField>
+
+      <SignInFormField
+        name='password'
+        validators={{ onChange: ({ value }) => (value.length <= 0 ? 'Cannot be empty' : undefined) }}
+      >
+        {field => (
+          <>
+            <label htmlFor={field.name} className='floating-label mt-12'>
+              <input
+                type='password'
+                id={field.name}
+                name={field.name}
+                placeholder='Password'
+                className='input input-primary input-xl w-full'
+                value={field.state.value}
+                onChange={e => field.handleChange(e.target.value)}
+              />
+              <span>Password</span>
+            </label>
+            <p role='alert' className='text-error h-[1em]'>
+              {field.state.meta.errors.join(', ')}
+            </p>
+          </>
+        )}
+      </SignInFormField>
+
       <button type='button' className='btn btn-primary btn-lg mt-12 w-full'>
         Sign In
       </button>
