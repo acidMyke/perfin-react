39463023a8586365ef46a1f2df0ecbd7dc4160d9
acidMyke <owner@acidmyke.link>
2025-08-06 20:09:03 +0800

    feat: auto redirection based on auth state

--- client/routes/signin.tsx
+++ client/routes/signin.tsx
@@ -1,9 +1,26 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router';
+import { whoamiQueryOptions } from '../queryOptions';
+import { queryClient } from '../trpc';
 
 export const Route = createFileRoute('/signin')({
   component: RouteComponent,
-})
+  validateSearch(search) {
+    return {
+      redirect: search.redirect,
+    };
+  },
+  async beforeLoad({ search }) {
+    const { isAuthenticated } = await queryClient.ensureQueryData(whoamiQueryOptions);
+    if (!isAuthenticated) {
+      return;
+    }
+    if (search.redirect)
+      throw redirect({
+        to: '/dashboard',
+      });
+  },
+});
 
 function RouteComponent() {
-  return <div>Hello "/signin"!</div>
+  return <div>Hello "/signin"!</div>;
 }
