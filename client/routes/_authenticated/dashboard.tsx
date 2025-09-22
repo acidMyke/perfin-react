b7efd19356841a42cf8b0c5eacac09aadcc93c8e
acidMyke <13977709+acidMyke@users.noreply.github.com>
2025-09-22 14:20:49 +0000

    refactor: extract expense insights as a compoenent

--- client/routes/_authenticated/dashboard.tsx
+++ client/routes/_authenticated/dashboard.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { whoamiQueryOptions } from '../../queryOptions';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { queryClient, trpc } from '../../trpc';
 import { PageHeader } from '../../components/PageHeader';
@@ -13,39 +12,48 @@ export const Route = createFileRoute('/_authenticated/dashboard')({
   },
 });
 
-function RouteComponent() {
-  const currencyFormatter = Intl.NumberFormat('en-SG', {
-    style: 'currency',
-    currency: 'SGD',
-  });
-  const { data: insightData } = useSuspenseQuery(trpc.dashboard.getInsights.queryOptions());
+const currencyFormatter = Intl.NumberFormat('en-SG', {
+  style: 'currency',
+  currency: 'SGD',
+});
 
+function RouteComponent() {
   return (
     <div className='mx-auto flex max-w-md flex-col'>
       <PageHeader title='Overview' />
+      <InsightsSection />
+    </div>
+  );
+}
+
+function InsightsSection() {
+  const { data } = useSuspenseQuery(trpc.dashboard.getInsights.queryOptions());
+
+  return (
+    <>
       <h2 className='mb-2 text-xl'>You have spent</h2>
 
       <div className='stats pl-4 shadow'>
         <div className='stat w-full'>
           <div className='state-title'>Last 7 days</div>
-          <div className='stat-value text-primary'>{currencyFormatter.format(insightData.lastSevenDays)}</div>
-          <div className={cn(`stat-desc`, { 'text-error': insightData.percentSevenDays > 0.2 })}>
-            <TrendIcon className='mr-2 inline-block' diff={insightData.percentSevenDays} />
-            <span>{currencyFormatter.format(insightData.diffSevenDays)} </span>
-            <span>({(insightData.percentSevenDays * 100).toFixed(2)}%)</span>
+          <div className='stat-value text-primary'>{currencyFormatter.format(data.lastSevenDays)}</div>
+          <div className={cn(`stat-desc`, { 'text-error': data.percentSevenDays > 0.2 })}>
+            <TrendIcon className='mr-2 inline-block' diff={data.percentSevenDays} />
+            <span>{currencyFormatter.format(data.diffSevenDays)} </span>
+            <span>({(data.percentSevenDays * 100).toFixed(2)}%)</span>
           </div>
         </div>
         <div className='stat w-full'>
           <div className='state-title'>Last 14 days</div>
-          <div className='stat-value text-primary'>{currencyFormatter.format(insightData.lastFourteenDays)}</div>
-          <div className={cn(`stat-desc`, { 'text-error': insightData.percentFourteenDays > 0.2 })}>
-            <TrendIcon className='mr-2 inline-block' diff={insightData.percentFourteenDays} />
-            <span>{currencyFormatter.format(insightData.diffFourteenDays)} </span>
-            <span>({(insightData.percentFourteenDays * 100).toFixed(2)}%)</span>
+          <div className='stat-value text-primary'>{currencyFormatter.format(data.lastFourteenDays)}</div>
+          <div className={cn(`stat-desc`, { 'text-error': data.percentFourteenDays > 0.2 })}>
+            <TrendIcon className='mr-2 inline-block' diff={data.percentFourteenDays} />
+            <span>{currencyFormatter.format(data.diffFourteenDays)} </span>
+            <span>({(data.percentFourteenDays * 100).toFixed(2)}%)</span>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
