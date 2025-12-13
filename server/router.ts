d9d3cb913ebcc9ead42f2a092da2ec23235f6e62
acidMyke <13977709+acidMyke@users.noreply.github.com>
2025-12-13 06:59:46 +0000

    feat: delete unused proof of concept procedure

--- server/router.ts
+++ server/router.ts
@@ -11,20 +11,6 @@ export const appRouter = router({
   dashboard: dashboardProcedure,
   expense: expenseProcedures,
   subject: subjectProcedures,
-  testapi: publicProcedure
-    .input(z.object({ name: z.string().optional() }).optional())
-    .query(({ input }) => (input?.name ? `Hello, ${input.name}` : 'Hello world')),
-  testauthapi: protectedProcedure.mutation(({ ctx: { user } }) => `Hello, ${user.name}`),
-  testPresignedUrl: protectedProcedure
-    .input(z.object({ files: z.array(z.object({ contentType: z.string(), filePath: z.string() })) }))
-    .mutation(
-      async ({ ctx, input }) =>
-        await Promise.all(
-          input.files.map(({ contentType, filePath }) =>
-            createSignedUrl(ctx, { method: 'PUT', contentType, filePath }),
-          ),
-        ),
-    ),
 });
 
 // For client to import just the types
