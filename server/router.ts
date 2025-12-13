55539f5e899492b7bad9f914eb49d3998ffd0f46
acidMyke <13977709+acidMyke@users.noreply.github.com>
2025-12-13 07:00:06 +0000

    feat: remove unused imports from router

--- server/router.ts
+++ server/router.ts
@@ -1,10 +1,8 @@
-import z from 'zod';
-import { protectedProcedure, publicProcedure, router } from './trpc';
+import { router } from './trpc';
 import { usersProcedures } from './features/users';
 import { expenseProcedures } from './features/expenses';
 import { subjectProcedures } from './features/subjects';
 import { dashboardProcedure } from './features/dashboard';
-import { createSignedUrl } from './lib/files';
 
 export const appRouter = router({
   ...usersProcedures,
