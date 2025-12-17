20df26767b8ff9069831988fd1e35ef2dddf3d2e
acidMyke <13977709+acidMyke@users.noreply.github.com>
2025-12-17 14:45:51 +0000

    feat: view total amount

--- client/routes/_authenticated/expenses/$expenseId/view.tsx
+++ client/routes/_authenticated/expenses/$expenseId/view.tsx
@@ -16,19 +16,21 @@ function RouteComponent() {
   const form = useExpenseForm();
 
   const expense = useStore(form.store, state => state.values);
+  const { ui, items } = expense;
+  const { grossAmount, expectedRefundSum, amount } = ui.calculateResult;
 
   return (
-    <div className='mx-auto grid max-w-md auto-cols-min auto-rows-auto grid-cols-1 gap-1 space-y-4 p-4'>
+    <div className='mx-auto grid max-w-md auto-cols-min auto-rows-auto grid-cols-1 gap-1 p-4'>
       <div className='border-base-300 col-span-2 space-y-1 border-b pb-2'>
         <h1 className='text-lg font-bold'>{expense.shopName ?? 'Unknown Shop'}</h1>
         {expense.shopMall && <p className='text-sm opacity-70'>{expense.shopMall}</p>}
         <p className='text-sm opacity-60'>{dateFormat.format(expense.billedAt)}</p>
       </div>
 
-      {expense.items.map(item => {
+      {items.map(item => {
         const { grossAmount, amount, minRefundCents } = calculateExpenseItem(item, expense);
         return (
-          <div key={item.id} className='border-base-300 col-span-2 grid grid-cols-subgrid border-b pb-2 last:border-0'>
+          <div key={item.id} className='border-base-300 col-span-2 grid grid-cols-subgrid pt-2 pb-2 last:border-0'>
             <span className='font-medium'>
               {item.name} ({formatCents(item.priceCents)}) × {item.quantity}
             </span>
@@ -50,6 +52,19 @@ function RouteComponent() {
           </div>
         );
       })}
+
+      <div className='border-t-base-content/20 col-span-2 grid grid-cols-subgrid border-t pt-4 text-xl *:odd:font-bold *:even:text-right'>
+        <span>Gross amount:</span>
+        <span>{currencyNumberFormat.format(grossAmount)}</span>
+        {expectedRefundSum > 0 && (
+          <>
+            <span>Expected total:</span>
+            <span>{currencyNumberFormat.format(grossAmount - expectedRefundSum)}</span>
+          </>
+        )}
+        <span>Total paid:</span>
+        <span>{currencyNumberFormat.format(amount)}</span>
+      </div>
     </div>
   );
 }
