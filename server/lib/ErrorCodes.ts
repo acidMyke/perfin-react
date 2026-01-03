d6441eed7e5167b5a21ff18b73fdf07ba9e9b1b8
acidMyke <13977709+acidMyke@users.noreply.github.com>
2026-01-03 06:14:06 +0000

    feat: handling of error codes

--- server/lib/ErrorCodes.ts
+++ server/lib/ErrorCodes.ts
@@ -1,8 +1,14 @@
-export const ErrorCodes = {
-  CSRF_FAILED: 'csrf_failed',
-  ELEVATION_REQUIRED: 'elevation_required',
-  SQLITE_ERROR: 'sqlite_error',
-  UNHANDLED_EXCEPTION: 'unhandled_exception',
-};
+export class ErrorCodes {
+  static readonly CSRF_FAILED = 'csrf_failed';
+  static readonly ELEVATION_REQUIRED = 'elevation_required';
+  static readonly SQLITE_ERROR = 'sqlite_error';
+  static readonly UNHANDLED_EXCEPTION = 'unhandled_exception';
+}
+
+export type ErrorCodesValues =
+  | typeof ErrorCodes.CSRF_FAILED
+  | typeof ErrorCodes.ELEVATION_REQUIRED
+  | typeof ErrorCodes.SQLITE_ERROR
+  | typeof ErrorCodes.UNHANDLED_EXCEPTION;
 
 export default ErrorCodes;
