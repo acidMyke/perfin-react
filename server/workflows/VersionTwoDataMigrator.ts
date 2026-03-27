3b4f6803328005dfc507903e352db15ca471aa84
acidMyke <owner@acidmyke.link>
2026-03-27 22:24:44 +0800

    feat: cycle checkpoint & inner cycle logging

--- server/workflows/VersionTwoDataMigrator.ts
+++ server/workflows/VersionTwoDataMigrator.ts
@@ -7,6 +7,10 @@ export type VersionTwoDataMigratorParam = {
   after?: string;
 };
 
+export type CycleCheckpointEvent = {
+  kill?: boolean;
+};
+
 export class VersionTwoDataMigrator extends WorkflowEntrypoint<Env, VersionTwoDataMigratorParam> {
   async run(event: WorkflowEvent<VersionTwoDataMigratorParam>, step: WorkflowStep) {
     const {
@@ -18,10 +22,37 @@ export class VersionTwoDataMigrator extends WorkflowEntrypoint<Env, VersionTwoDa
       this.logToDiscord(`Starting migration from ID: ${after}`, { maxCount, maxCycle, maxDelay, instanceId });
     });
 
+    const cyclePerNoti = Math.ceil(maxCycle / 50);
     let lastSeenId = after;
     let curCycle = 0;
+    let idProcSince: string[] = [];
     for (; curCycle < event.payload.maxCycle; curCycle++) {
       // TODO
+      await step.sleep('simulated migration', 50_000);
+
+      const shouldNotify = cyclePerNoti <= 1 || curCycle % cyclePerNoti === 0;
+
+      if (shouldNotify) {
+        await step.do(`noti-batch-${curCycle}`, async () => {
+          this.logToDiscord(`${instanceId} proccessed count: ${idProcSince.length}`, idProcSince);
+        });
+        idProcSince = [];
+      }
+
+      try {
+        // Dual purpose: Delay & Kill switch
+        const checkpointEvent = await step.waitForEvent<CycleCheckpointEvent>(`cycle-checkpoint-${curCycle}`, {
+          type: 'cycle-checkpoint',
+          timeout: maxDelay,
+        });
+
+        if (checkpointEvent.payload.kill) {
+          console.warn('kill signal recieved');
+          break;
+        }
+      } catch (e: unknown) {
+        console.log(e);
+      }
     }
 
     await step.do('noti-end', async () => {
@@ -29,7 +60,7 @@ export class VersionTwoDataMigrator extends WorkflowEntrypoint<Env, VersionTwoDa
     });
   }
 
-  private async logToDiscord(message: string, data: unknown): Promise<void> {
+  private async logToDiscord(message: string, data?: unknown): Promise<void> {
     const serializedData = data ? '```json\n' + JSON.stringify(data, null, 2) + '\n```' : '';
     const content = `${message}\n${serializedData}`;
     const response = await fetch(this.env.DISCORD_WEBHOOK_URL, {
