111a172417a6a9e126a01e670ea7f14abac355fc
acidMyke <owner@acidmyke.link>
2026-04-03 11:18:14 +0800

    feat: update trigram chunking expression

--- server/lib/utils.ts
+++ server/lib/utils.ts
@@ -1,16 +1,21 @@
 export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
-export const getTrigrams = (input: string) =>
-  input
+export function getTrigrams(input: string) {
+  const phrases = input
     .trim()
-    .split(/\s+/)
-    .flatMap(phrase =>
-      phrase.length < 3
-        ? phrase.toLowerCase()
-        : Array.from({ length: Math.min(phrase.length, 10) }).map((_, i) =>
-            phrase.slice(Math.max(i - 2, 0), i + 1).toLowerCase(),
-          ),
-    );
+    .toLowerCase()
+    .split(/[^a-zA-Z0-9'-]+/);
+  const chunks: string[] = [];
+  for (const phrase of phrases) {
+    if (!phrase) continue;
+    const numChunk = Math.min(phrase.length, 10);
+    for (let idx = 0; idx < numChunk; idx++) {
+      chunks.push(phrase.slice(Math.max(idx - 2, 0), idx + 1));
+    }
+  }
+
+  return chunks;
+}
 
 // Distance in deg for singapore
 export const GRID_SIZE = 0.002; // 222 meters
