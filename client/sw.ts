fe5624c608fc45c2ddbd91e8696fd04762f3ed5c
acidMyke <owner@acidmyke.link>
2026-06-13 14:43:03 +0800

    feat: service worker entry point and notification event schema

---
+++ client/sw.ts
@@ -0,0 +1,41 @@
+/// <reference lib="WebWorker" />
+
+import { NotificationEventDataSchema } from '#server/lib/notification';
+
+declare const self: ServiceWorkerGlobalScope;
+self.addEventListener('push', event => {
+  if (!event.data) return;
+  try {
+    const { title = 'Perfin', ...otherOptions } = NotificationEventDataSchema.parse(event.data.json());
+    const options: NotificationOptions = {
+      ...otherOptions,
+      icon: '/android-chrome-192x192.png',
+      badge: '/android-chrome-192x192.png',
+    };
+    event.waitUntil(self.registration.showNotification(title, options));
+  } catch (err) {
+    if (err) {
+      console.error(err);
+    }
+  }
+});
+
+self.addEventListener('notificationclick', (event: NotificationEvent) => {
+  event.notification.close();
+  const targetUrl = event.notification.data?.targetUrl || '/';
+  const absoluteTargetUrl = new URL(targetUrl, self.location.origin).href;
+  event.waitUntil(
+    self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
+      for (const client of clientList) {
+        if ('navigate' in client && 'focus' in client) {
+          return client.navigate(absoluteTargetUrl).then(focusedClient => {
+            if (focusedClient) return focusedClient.focus();
+          });
+        }
+      }
+      if ('openWindow' in self.clients) {
+        return self.clients.openWindow(absoluteTargetUrl);
+      }
+    }),
+  );
+});
---
+++ server/lib/notification.ts
@@ -0,0 +1,30 @@
+import * as z from 'zod/v4-mini';
+
+export const NotificationDataOptionsSchema = z.looseObject({
+  targetUrl: z.optional(z.string()),
+});
+
+export const NotificationActionSchema = z.object({
+  action: z.string(),
+  title: z.string(),
+  icon: z.optional(z.string()),
+});
+
+export const NotificationOptionsSchema = z.object({
+  body: z.optional(z.string()),
+  image: z.optional(z.string()),
+  tag: z.optional(z.string()),
+  data: z.optional(NotificationDataOptionsSchema),
+  vibrate: z.optional(z.union([z.array(z.number()), z.number()])),
+  timestamp: z.optional(z.number()),
+  requireInteraction: z.optional(z.boolean()),
+  renotify: z.optional(z.boolean()),
+  silent: z.optional(z.boolean()),
+  actions: z.optional(z.array(NotificationActionSchema)),
+});
+
+export const NotificationEventDataSchema = z.extend(NotificationOptionsSchema, { title: z.string() });
+
+export type NotificationAction = z.infer<typeof NotificationActionSchema>;
+export type NotificationOptions = z.infer<typeof NotificationOptionsSchema>;
+export type NotificationEventData = z.infer<typeof NotificationEventDataSchema>;
