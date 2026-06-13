3632bc42f9123e831c6c7c78ecf0f90f63ffba74
acidMyke <owner@acidmyke.link>
2026-06-13 17:24:50 +0800

    feat: db schema & migrations notification columns

---
+++ db/migrations/d1-prod/005_V003_2.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `user_devices` ADD `show_notification` integer;
+ALTER TABLE `user_devices` ADD `push_endpoint` text;
+ALTER TABLE `user_devices` ADD `push_p256dh` text;
+ALTER TABLE `user_devices` ADD `push_auth` text;
