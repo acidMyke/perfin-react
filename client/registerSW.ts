fa6024d1aefa29174ae1b3bf564f4f6e430bd354
acidMyke <owner@acidmyke.link>
2026-06-13 15:17:26 +0800

    feat: snippet for subscribing to web push

--- client/registerSW.ts
+++ client/registerSW.ts
@@ -1,12 +1,38 @@
 import swUrl from './sw?worker&url';
 
+async function registerServiceWorkerInternally() {
+  try {
+    return await navigator.serviceWorker.register(swUrl, { type: 'module' });
+  } catch (error) {
+    console.error('Service Worker registration failed:', error);
+    throw error;
+  }
+}
+
 export async function registerServiceWorker() {
   if (!('serviceWorker' in navigator)) return;
-  window.addEventListener('load', async () => {
-    try {
-      await navigator.serviceWorker.register(swUrl, { type: 'module' });
-    } catch (error) {
-      console.error('Service Worker registration failed:', error);
+  window.addEventListener('load', () => registerServiceWorkerInternally());
+}
+
+export async function subscribeToPush() {
+  try {
+    const registration = await registerServiceWorkerInternally();
+    let subscription = await registration.pushManager.getSubscription();
+
+    if (!subscription) {
+      subscription = await registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: import.meta.env.VITE_VAPID_PUBLIC_KEY,
+      });
     }
-  });
+
+    return {
+      endpoint: subscription.endpoint,
+      key: subscription.getKey('p256dh'),
+      auth: subscription.getKey('auth'),
+    };
+  } catch (error) {
+    console.error('Service Worker registration / Push subscription failed:', error);
+    throw error;
+  }
 }
